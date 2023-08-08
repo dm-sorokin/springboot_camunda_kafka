5985d558a634bbf4b6fdd5c8eecc47eab1fc2811
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-08-08 12:08:43 +0300

    int test MessageController

--- src/main/java/ru/dmitry/springboot_camunda_kafka/controller/MessageController.java
+++ src/main/java/ru/dmitry/springboot_camunda_kafka/controller/MessageController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.dmitry.springboot_camunda_kafka.dto.MessageDto;
 
+
 @RestController
 @RequestMapping("/kafka")
 @RequiredArgsConstructor
